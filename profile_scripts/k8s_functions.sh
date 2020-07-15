e6b31901fecf07e15cca6484a67fde32683ecc96
esnow <edward.snow@disneystreaming.com>
2020-07-15 09:16:58 +0100

    add k8s functions

--- profile_scripts/k8s_functions.sh
+++ profile_scripts/k8s_functions.sh
@@ -1,15 +1,15 @@
 function rek-minikube-auth() {
 	kubectl config set-cluster rek-minikube --server=https://kubernetes.default.svc.cluster.local:8080
 	kubectl config set-context rek-minikube --cluster=rek-minikube --namespace=default --user=rek-minikube
-	kubectl config set-cluster rek-minikube --certificate-authority='/home/eddy/rek-minikube/ca.crt'
+	kubectl config set-cluster rek-minikube --certificate-authority='/home/eddy/keys/rek-minikube/ca.crt'
 
 	#kubectl config use-context rek-minikube
 	curl -s -X POST "https://keycloak.esnow.uk/auth/realms/master/protocol/openid-connect/token" \
 		-d 'grant_type=password' \
 		-d 'client_id=kubernetes_minikube' \
-		-d 'client_secret=$(cat ~/.kube/.rek-minikube-client-secret)' \
+		-d "client_secret=$(cat ~/.kube/.rek-minikube-client-secret)" \
 		-d 'username=eddysnow@gmail.com' \
-		-d 'password=$(cat ~/.kube/.rek-minikube-psw)' \
+		-d "password=$(cat ~/.kube/.rek-minikube-psw)" \
 		-d 'scope=openid' \
 		-d 'reponse_type=id_token' > ~/temp/jwt.token
 
