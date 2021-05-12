ee8cb68846476fe93c4ebed8d4ce17897f7a77f8
esnow <edward.snow@disneystreaming.com>
2021-05-12 09:31:39 +0100

    add git functions

---
+++ profile_scripts/git_functions.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+function convert_public_github() {
+    REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
+    if [ -z "$REPO_URL" ]; then
+    echo "-- ERROR:  Could not identify Repo url."
+    echo "   It is possible this repo is already using SSH instead of HTTPS."
+    fi
+
+    USER=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*)#\1#p'`
+    if [ -z "$USER" ]; then
+    echo "-- ERROR:  Could not identify User."
+    fi
+
+    REPO=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*)#\2#p'`
+    if [ -z "$REPO" ]; then
+    echo "-- ERROR:  Could not identify Repo."
+    fi
+
+    NEW_URL="git@github.com:$USER/$REPO.git"
+    echo "Changing repo url from "
+    echo "  '$REPO_URL'"
+    echo "      to "
+    echo "  '$NEW_URL'"
+    echo ""
+
+    git remote set-url origin $NEW_URL
+
+    echo "Success"
+}
